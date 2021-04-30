f87758752e1ff4ee7c99166077b3b4398a9fae00
kwonlauren <kwonlauren@snu.ac.kr>
2021-04-30 16:57:28 +0900

    infix_to_postfix including ~ test

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -24,7 +24,7 @@ public class CalculatorTest {
     }
 
     private static void command(String input) {
-    //test
+    //testt
     }
 
     private static String infix_to_postfix(String infix) {
